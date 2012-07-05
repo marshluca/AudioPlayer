db04c32a15bb3f5812c8df90c9294e61448f01f6
Lin Zhang <marshluca@gmail.com>
2012-07-05 11:36:04 +0800

    move the library files into AudioPlayer

---
+++ AudioPlayer/AudioPlayer.h
@@ -0,0 +1,29 @@
+//
+//  AudioPlayer.h
+//  Share
+//
+//  Created by Lin Zhang on 11-4-26.
+//  Copyright 2011年 www.eoemobile.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class AudioButton;
+@class AudioStreamer;
+
+@interface AudioPlayer : NSObject {
+    AudioStreamer *streamer;
+    AudioButton *button;   
+    NSURL *url;
+    NSTimer *timer;
+}
+
+@property (nonatomic, retain) AudioStreamer *streamer;
+@property (nonatomic, retain) AudioButton *button;
+@property (nonatomic, retain) NSURL *url;
+
+- (void)play;
+- (void)stop;
+- (BOOL)isProcessing;
+
+@end
