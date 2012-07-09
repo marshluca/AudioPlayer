e927908c472819b647ebe9f5f390b78974125044
Lin Zhang <marshluca@gmail.com>
2012-07-09 22:10:43 +0800

    declare a table view in ViewController

--- AudioPlayerDemo/AudioPlayerDemo/ViewController.h
+++ AudioPlayerDemo/AudioPlayerDemo/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @end
--- AudioPlayerDemo/AudioPlayerDemo/ViewController.m
+++ AudioPlayerDemo/AudioPlayerDemo/ViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "ViewController.h"
+#import "AudioCell.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+
+@synthesize tableView;
 
 - (void)viewDidLoad
 {
@@ -24,11 +27,46 @@
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    self.tableView = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+#pragma mark
+#pragma mark - UITableView Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *CellIdentifier = [NSString stringWithFormat:@"Cell%d%d",indexPath.section, indexPath.row];
+    
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+	if (cell == nil) {
+		cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier] autorelease];
+	}
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // to be rewrite in subclass    
 }
 
 @end
